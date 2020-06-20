c440366809e50809ea62a6aca68117326c0a3fc3
Viraj Khatri <virajkhatri@gmail.com>
2020-06-20 22:41:28 +0530

    Refactoring code te be legible

--- ffind.go
+++ ffind.go
@@ -12,20 +12,9 @@ func main() {
 	if len(args) < 3 {
 		printUsage()
 	}
+	args = getArgs(args)
 
-	var opts string
-	if args[0][0] == '-' {
-		switch args[0][1] {
-			case 'd':
-				opts = "d"
-			case 'f':
-				opts = "f"
-			default:
-				opts = "f"
-		}
-	}
-
-	cmd := exec.Command("find", args[2], "-type", opts, "-name", args[1])
+	cmd := exec.Command("find", args...)
 	stdout, outErr := cmd.Output()
 	if outErr != nil {
 		panic(outErr)
@@ -34,6 +23,31 @@ func main() {
 }
 
 func printUsage() {
-	fmt.Fprintln(os.Stderr, "find [-OPTION] ['FILENAME'] [PATH]")
+	fmt.Fprintln(os.Stderr, "find -OPTION 'FILENAME' PATH")
 	os.Exit(1)
+}
+
+func getArgs(osArgs []string) []string {
+	var opts []string
+
+	opts = append(opts, osArgs[2])
+
+	opts = append(opts, "-type")
+	if osArgs[0][0] == '-' {
+		switch osArgs[0][1] {
+		case 'd':
+			opts = append(opts, "d")
+		case 'f':
+			opts = append(opts, "f")
+		default:
+			opts = append(opts, "f")
+		}
+	} else {
+		printUsage()
+	}
+
+	opts = append(opts, "-name")
+	opts = append(opts, osArgs[1])
+
+	return opts
 }
