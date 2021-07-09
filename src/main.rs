e9c5e9f649c716edb81e19b27f27f0483e72ed3b
Viraj Khatri <virajkhatri@gmail.com>
2021-07-10 01:52:19 +0530

    Adding regex, putting preliminary regex code in

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::env;
 use std::process;
 use std::io;
+use regex::Regex;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -14,7 +15,8 @@ fn main() {
         },
         Ok(cmd_args) => cmd_args,
     };
-
+    
+/*
     // executing the command
     let exec_status_result = process::Command::new("find")
         .args(cmd_args)
@@ -39,6 +41,7 @@ fn main() {
             2
         },
     });
+*/
 }
 
 fn print_usage() {
@@ -48,7 +51,24 @@ fn print_usage() {
 fn make_command(args_in: Vec<String>) -> Result<Vec<String>, io::Error> {
     let mut args_out = Vec::new();
 
-    let sorted_args = sort_args(args_in)?;
+    for (arg_no, arg) in args_in.iter().enumerate() {
+        let re = Regex::new(r"^(?P<flags>--?)([?P<separator>^=]+)(?P<strings>.*?)$").unwrap();
+        println!("===== arg={:?} =====", &arg);
+
+        // let regex_arg: Vec<String> = re.find_iter(&arg).filter_map(|cap| cap.as_str().parse().ok()).collect();
+        // println!("regex_arg={:?}", regex_arg)
+        // Borked : this is giving only cap[0], the full string, it's not collecting the regex matches
+
+        match re.captures(&arg) {
+            Some(caps) => {
+                let mut regexed_args: Vec<String> = caps.iter().;  // How - To
+                println!("args={:?}", regexed_args);
+            }
+            None => (),
+        };
+        
+       // let sorted_args = sort_args(args_in)?;
+    }
     Ok(args_out)
 }
 
@@ -69,7 +89,7 @@ fn sort_args(args_in: Vec<String>) -> Result<SortedArgs, io::Error> {
     let mut exec_args = String::from("");
 
     short_args = get_short_args(args_in)?;
-    long_args = get_long_args(args_in)?;
+    // long_args = get_long_args(args_in)?;
 
     let mut sorted_args = SortedArgs {
         short_args: short_args,
@@ -86,20 +106,20 @@ fn sort_args(args_in: Vec<String>) -> Result<SortedArgs, io::Error> {
 fn get_short_args(args_in: Vec<String>) -> Result<Vec<String>, io::Error> {
     let mut ret_short_args: Vec<String> = Vec::new();
 
-    for args in args_in {
-        let args_c = args.char_indices();
-        if args_c.count() > 1 { // short flags or long flags
-            if args_c.next().unwrap() == (0, '-') {
-                let second_args_char = args_c.next().unwrap();
-                if second_args_char == (1, '-') { // second dash so long flag
-                    continue;
-                } else {
-                    ret_short_args.push(second_args_char.1.to_string());
-                    return Ok(ret_short_args);
-                }
-            }
-        }
-    }
+    // for args in args_in {
+    //     let args_c = args.char_indices();
+    //     if args_c.count() > 1 { // short flags or long flags
+    //         if args_c.next().unwrap() == (0, '-') {
+    //             let second_args_char = args_c.next().unwrap();
+    //             if second_args_char == (1, '-') { // second dash so long flag
+    //                 continue;
+    //             } else {
+    //                 ret_short_args.push(second_args_char.1.to_string());
+    //                 return Ok(ret_short_args);
+    //             }
+    //         }
+    //     }
+    // }
     return Ok(ret_short_args);
 }
 
